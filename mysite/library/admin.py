f293433cdbf1421b20266a92f24061a587533c47
DonatasNoreika <donoras@gmail.com>
2024-01-09 10:35:55 +0200

    admin.py aprašytos AuthorAdmin ir BookInstanceAdmin klasės

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
 from .models import Genre, Book, Author, BookInstance
 
+
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'isbn', 'display_genre']
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name']
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'book', 'status', 'due_back']
+    list_filter = ['status', 'due_back']
+
+    fieldsets = (
+        ('General', {'fields': ('uuid', 'book')}),
+        ('Availability', {'fields': ('status', 'due_back')}),
+    )
 
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
-admin.site.register(Author)
-admin.site.register(BookInstance)
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(BookInstance, BookInstanceAdmin)
