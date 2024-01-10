946268dce356673d7ff09398969657f51757506f
DonatasNoreika <donoras@gmail.com>
2024-01-10 09:54:04 +0200

    Sukurtas HOME puslapis

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import BookInstance, Book, Author, Genre
+import datetime
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Labas, pasauli")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact='g').count()
+    num_authors = Author.objects.all().count()
+    my_context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "num_authors": num_authors,
+        "now": datetime.datetime.today(),
+    }
+    return render(request, template_name='index.html', context=my_context)
