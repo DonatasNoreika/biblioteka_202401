6100a3b91f376feb23ed5e631f8501ff41fd36a0
DonatasNoreika <donoras@gmail.com>
2024-01-15 09:59:22 +0200

    +Įdėtas puslapiavimas autorių sąraše

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import BookInstance, Book, Author, Genre
 import datetime
 from django.views import generic
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -22,8 +23,11 @@ def index(request):
 
 def authors(request):
     authors = Author.objects.all()
+    paginator = Paginator(authors, per_page=4)
+    page_number = request.GET.get('page')
+    paged_authors = paginator.get_page(page_number)
     context = {
-        'authors': authors,
+        'authors': paged_authors,
     }
     return render(request, template_name="authors.html", context=context)
 
