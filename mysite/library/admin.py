c1305c38dab3e264d6cb8e005d1dfb5f2a52a4a7
DonatasNoreika <donoras@gmail.com>
2024-01-18 10:52:11 +0200

    Prie knygos atvaizduojami komentarai

--- mysite/library/admin.py
+++ mysite/library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Genre, Book, Author, BookInstance
+from .models import Genre, Book, Author, BookInstance, BookReview
 
 
 class BookInstanceInLine(admin.TabularInline):
@@ -30,8 +30,12 @@ class BookInstanceAdmin(admin.ModelAdmin):
     )
 
 
+class BookReviewAdmin(admin.ModelAdmin):
+    list_display = ['book', 'date_created', 'reviewer', 'content']
+
 # Register your models here.
 admin.site.register(Genre)
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author, AuthorAdmin)
 admin.site.register(BookInstance, BookInstanceAdmin)
+admin.site.register(BookReview, BookReviewAdmin)
