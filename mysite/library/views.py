0a105011137f1977d3582624d027c00e05e75957
DonatasNoreika <donoras@gmail.com>
2024-01-22 10:33:40 +0200

    Įgyvendintas vartotojo ir jo profilio redagavimas

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -10,7 +10,7 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth.forms import User
 from django.views.generic.edit import FormMixin
-from .forms import BookReviewForm
+from .forms import BookReviewForm, UserUpdateForm, ProfileUpdateForm
 from django.contrib.auth.decorators import login_required
 
 
@@ -138,4 +138,19 @@ def register(request):
 
 @login_required()
 def profile(request):
-    return render(request, template_name="profile.html")
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, 'Profilis atnaujintas')
+            return redirect("profile")
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+        context = {
+            "u_form": u_form,
+            "p_form": p_form,
+        }
+        return render(request, template_name="profile.html", context=context)
